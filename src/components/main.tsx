7e8b6a5ff2b81d0e43adf050d890b1f6e563c80b
Owais <warsimuhammadowais@gmail.com>
2024-06-26 21:46:00 +0530

    add product hunt badge

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -66,27 +66,8 @@ export default function Main() {
                     ) : null}
                 </div>
 
-                <div className="fixed bottom-4 right-4">
-                    <a
-                        href="https://github.com/Muhammad-Owais-Warsi/NPM-Suggester"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="px-4 py-2 bg-gray-800 text-white font-semibold rounded-md hover:bg-gray-900 transition duration-300 flex items-center"
-                    >
-                        <svg
-                            className="w-6 h-6 mr-2"
-                            fill="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.865 8.166 6.839 9.493.5.092.682-.218.682-.48 0-.237-.008-.866-.013-1.701-2.782.604-3.37-1.34-3.37-1.34-.454-1.15-1.108-1.457-1.108-1.457-.906-.618.069-.606.069-.606 1.002.07 1.532 1.028 1.532 1.028.893 1.53 2.345 1.087 2.916.83.092-.646.35-1.087.636-1.337-2.226-.252-4.566-1.113-4.566-4.946 0-1.092.39-1.985 1.032-2.684-.103-.252-.448-1.27.097-2.647 0 0 .84-.268 2.75 1.025a9.58 9.58 0 0 1 2.51-.34c.853.003 1.713.114 2.511.34 1.908-1.293 2.747-1.025 2.747-1.025.548 1.377.202 2.395.1 2.647.643.699 1.03 1.592 1.03 2.684 0 3.842-2.344 4.691-4.576 4.936.36.308.681.922.681 1.857 0 1.34-.012 2.421-.012 2.75 0 .266.18.578.688.479A12.012 12.012 0 0 0 22 12c0-5.52-4.48-10-10-10z"
-                            />
-                        </svg>
-                        Star on GitHub
-                    </a>
+                <div className="fixed bottom-4 right-4 shadow-md">
+                <a href="https://www.producthunt.com/posts/npm-suggester?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-npm&#0045;suggester" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=466189&theme=light" alt="NPM&#0032;Suggester - Get&#0032;quick&#0032;recommendations&#0032;for&#0032;NPM&#0032;packages&#0046; | Product Hunt" style={{width: "250px", height:"54px"}} /></a>
                 </div>
             </div>
         </div>
