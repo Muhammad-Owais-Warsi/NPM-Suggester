b6675ef1479fd35ea7f5d69c57415748fa7fc2c9
Prajapati Shrikrushna R. <Krishnaprajapati757@gmail.com>
2024-07-03 17:31:34 +0530

    contributor page

--- src/components/response.tsx
+++ src/components/response.tsx
@@ -1,28 +1,40 @@
-
+import React from "react";
 
 interface PropType {
-    prop:{
-        no : number,
-        package : string,
-        description : string,
-        link : string
-    }
+    prop: {
+        no: number;
+        package: string;
+        description: string;
+        link?: string; // Make link optional
+    };
 }
 
-export default function ResponseBlock({prop} : PropType) {
+
+const ResponseBlock: React.FC<PropType> = ({ prop }) => {
     return (
         <div className="flex justify-center align-middle shadow-md p-4 mb-4">
-        <div className="flex flex-col w-full">
-            <div className="flex items-center mb-2">
-                <p className="text-lg font-bold">{prop.no}.</p>
-                <a href={prop.link} className="underline"> 
-                    <span className="ml-2 text-gray-600 font-bold text-xl">{prop.package}</span>
-                </a>
-            </div>
-            <div className="flex flex-col">
-                <p className="mb-2">{prop.description}</p>
+            <div className="flex flex-col w-full">
+                <div className="flex items-center mb-2">
+                    <p className="text-lg font-bold">{prop.no}.</p>
+                    {prop.link ? (
+                        <a
+                            href={prop.link}
+                            className="underline ml-2 text-gray-600 font-bold text-xl"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {prop.package}
+                        </a>
+                    ) : (
+                        <span className="ml-2 text-gray-600 font-bold text-xl">{prop.package}</span>
+                    )}
+                </div>
+                <div className="flex flex-col">
+                    <p className="mb-2">{prop.description}</p>
+                </div>
             </div>
         </div>
-    </div>
-    )
-}
+    );
+};
+
+export default ResponseBlock;
