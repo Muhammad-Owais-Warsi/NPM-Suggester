244307fc4f4682023bce9c8d406a51011b497504
Mehak <mehakmattoo7@gmail.com>
2024-07-17 16:47:49 +0530

    adjusted history section height

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -185,7 +185,7 @@ const Main: React.FC = () => {
             className="mb-6"
           >
             <input
-              className="block w-full rounded-md border-0 py-2.5 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-blue-500 px-2"
+              className="block text-sm md:text-base w-full rounded-md border-0 py-2.5 text-gray-900 dark:text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-blue-500 px-2"
               placeholder="Enter query"
               value={query}
               onChange={(e) => handleInputChange(e.target.value)}
